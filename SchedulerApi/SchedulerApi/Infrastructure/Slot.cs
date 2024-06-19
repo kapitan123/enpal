16ff476e047420827277bbe9521e2de02d0f2051
AnKlimenko <lol@gkek.com>
2024-06-19 12:18:08 +0400

    Add DB models

---
+++ SchedulerApi/SchedulerApi/Infrastructure/Slot.cs
@@ -0,0 +1,10 @@
+﻿namespace SchedulerApi.Infrastructure;
+
+public class Slot
+{
+	public int Id { get; set; }
+	public DateTime StartDate { get; set; }
+	public DateTime EndDate { get; set; }
+	public bool Booked { get; set; }
+	public int SalesManagerId { get; set; }
+}
