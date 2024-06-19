0a910a13cc1a1ad066cc5571920b286d027b8a9a
AnKlimenko <lol@gkek.com>
2024-06-19 12:24:08 +0400

    Add connection strings

--- SchedulerApi/SchedulerApi/Program.cs
+++ SchedulerApi/SchedulerApi/Program.cs
@@ -1,8 +1,15 @@
+using System.Data;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
